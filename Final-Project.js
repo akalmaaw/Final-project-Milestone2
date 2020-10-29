2341e88b2129aab367a2abba4806516cc8b0643d
Al-Faisal Al-Maawali <akalmaaw@asu.edu>
2020-10-28 20:35:40 -0700

    Inslude photos and vedio with their links

--- Final-Project.js
+++ Final-Project.js
@@ -1,7 +1,7 @@
-//var value1=window.prompt("please enter your name")
-// document.write(value1 + ", welcome to exercise website");
- //document.write(" ");
- //var value2=window.prompt("please enter your age")
- //document.write("  age :"+value2 );
+ var value1=window.prompt("please enter your name")
+ document.write(value1 + ", welcome to exercise website");
+ document.write(" ");
+ var value2=window.prompt("please enter your age")
+ document.write("  age :"+value2 );
 
 function click(){}
